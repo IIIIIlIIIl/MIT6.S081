fb648cf1fcd63a3dea0107649a7b077208b40123
IIIIIlIIIl <m2824168336@outlook.com>
2026-05-21 17:05:41 +0800

    finish syscalls lab

--- kernel/proc.c
+++ kernel/proc.c
@@ -288,6 +288,7 @@ fork(void)
     return -1;
   }
   np->sz = p->sz;
+  np->trace_mask=p->trace_mask;
 
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
@@ -653,4 +654,13 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+int proccount(void){
+  struct proc *p;
+  int number=0;
+  for(p=proc;p<&proc[NPROC];p++){
+    if(p->state!=UNUSED)number++;
+  }
+  return number;
 }
